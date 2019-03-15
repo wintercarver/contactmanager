faa469e1e2132f864ec56e796ac1eaf7c276e788
kevin connolly <kevin@preferred.jp>
2019-03-15 21:18:24 +0900

    lecture 20

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-666-5555" />
-        <Contact name="Jane" email="jane@gmail.com" phone="777-666-5555" />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-666-5555"
+          />
+          <Contact
+            name="Jane Does"
+            email="jane@gmail.com"
+            phone="777-666-5555"
+          />
+        </div>
       </div>
     );
   }
