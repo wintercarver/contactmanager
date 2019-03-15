b1d4566587c36ce059e2448a874ab08edead6ed7
kevin connolly <kevin@preferred.jp>
2019-03-15 20:56:34 +0900

    lecture 17

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-666-5555" />
+        <Contact name="Jane" email="jane@gmail.com" phone="777-666-5555" />
       </div>
     );
   }
