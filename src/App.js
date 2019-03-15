2293277860d40e1eeaf03d0eb2e1b09cb4dbd07b
kevin connolly <kevin@preferred.jp>
2019-03-15 20:48:38 +0900

    lecture 16

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <Contact />
-        <Contact />
+        <Header />
         <Contact />
       </div>
     );
