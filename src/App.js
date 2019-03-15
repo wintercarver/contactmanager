73e6629874c9d0fd8fdf416e77c826a0e35b2d36
kevin connolly <kevin@preferred.jp>
2019-03-15 20:36:49 +0900

    lecture 14

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    // // JSX example code for same result
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "The App Component")
+    // );
+    const name = "Larry";
+    const showHello = false;
+    const showMath = true;
+
+    let math;
+    if (showMath) {
+      math = <h4>1 + 1 = {1 + 1}</h4>;
+    } else {
+      math = null;
+    }
     return (
       <div className="App">
         <h1>The App Component</h1>
+        <h4>Hello {name} </h4>
+        {math}
       </div>
     );
   }
