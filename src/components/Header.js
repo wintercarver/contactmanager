45239c65d560a9afb76dd7b80242b66098ae63f6
kevin connolly <kevin@preferred.jp>
2019-03-15 21:07:00 +0900

    lecture 19

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => {
   const { branding } = props;
   return (
     <div>
-      <h1>{branding}</h1>
+      <h1 style={headingStyle}>{branding}</h1>
     </div>
   );
 };
@@ -18,4 +18,8 @@ Header.propTypes = {
   branding: PropTypes.string.isRequired
 };
 
+const headingStyle = {
+  color: "green",
+  fontSize: "50px"
+};
 export default Header;
