914de8a4f25f7f00b4c94f3796fb76644a936a0c
kevin connolly <kevin@preferred.jp>
2019-03-17 15:39:45 +0900

    Updates

---
+++ precache-manifest.37f52b810214ff9c77beeb7160e85a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba1213e95033b353100a",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "ba1213e95033b353100a",
+    "url": "/contactmanager/static/js/main.d1ec33f7.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "6de76a7a4f9f13e0728b",
+    "url": "/contactmanager/static/css/2.b561c598.chunk.css"
+  },
+  {
+    "revision": "6de76a7a4f9f13e0728b",
+    "url": "/contactmanager/static/js/2.3deb431d.chunk.js"
+  },
+  {
+    "revision": "43768673ccccc8ccb4cb1b9f7484f7cb",
+    "url": "/contactmanager/index.html"
+  }
+];
