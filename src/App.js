bfdc0d6c480a4a7228c835dd2025c9b8d0ac2265
kevin connolly <kevin@preferred.jp>
2019-03-17 15:08:49 +0900

    lesson 40

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
 import Header from "./components/layout/Header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "./context";
@@ -22,6 +23,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
